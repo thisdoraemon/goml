223d3e8807c026417992c73940f16de2835424d3
thisdoraemon <ezrantn.dev@gmail.com>
2024-01-05 19:35:46 +0700

    issue: testpredictedprice mismatch result

--- test/linear_regression_test.go
+++ test/linear_regression_test.go
@@ -1,13 +1,14 @@
 package test
 
 import (
-	"github.com/thisdoraemon/goml/regression"
 	"math"
 	"testing"
+
+	"github.com/thisdoraemon/goml/regression"
 )
 
 func almostEqual(a, b float64) bool {
-	const epsilon = 1e-2
+	epsilon := 0.0001
 	return math.Abs(a-b) < epsilon
 }
 
@@ -39,29 +40,17 @@ func TestMean(t *testing.T) {
 }
 
 func TestPredictedPrice(t *testing.T) {
-	sizes := []float64{1400, 1600, 1700, 1875, 1100, 1550, 2350, 2450, 1425, 1700}
-	prices := []float64{245, 312, 279, 308, 199, 219, 405, 324, 319, 255}
-	houseSize := 2000.0
+	x := []float64{1, 2, 3, 4, 5}
+	y := []float64{2, 4, 5, 4, 5}
 
-	expectedIntercept := 67.62
-	expectedSlope := 0.13
-	expectedPredictedPrice := 309.62
+	meanX := regression.Mean(x)
+	meanY := regression.Mean(y)
 
-	meanSize := regression.Mean(sizes)
-	meanPrice := regression.Mean(prices)
-	beta1 := regression.CalculateSlope(sizes, meanSize, prices, meanPrice)
-	beta0 := meanPrice - (beta1 * meanSize)
+	slope := regression.CalculateSlope(x, meanX, y, meanY)
 
-	if !almostEqual(beta0, expectedIntercept) {
-		t.Errorf("Expected intercept: %v, got: %v", expectedIntercept, beta0)
-	}
-	if !almostEqual(beta1, expectedSlope) {
-		t.Errorf("Expected slope: %v, got: %v", expectedSlope, beta1)
-	}
+	predictedPrice := slope*(6-meanX) + meanY
 
-	predictedPrice := beta0 + (beta1 * houseSize)
-
-	if !almostEqual(predictedPrice, expectedPredictedPrice) {
-		t.Errorf("Expected predicted price: %v, got: %v", expectedPredictedPrice, predictedPrice)
+	if predictedPrice != 4.5 {
+		t.Errorf("predicted price for x = 6 is incorrect. Got %f, expected %f", predictedPrice, 4.5)
 	}
 }
